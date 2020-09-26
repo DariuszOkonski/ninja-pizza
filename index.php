af145cf55a8c650ca6a6128529e648b98fbdef7a
Dariusz Okonski <darek200180@gmail.com>
2020-09-26 15:18:24 +0100

    add content to files header and footer

--- index.php
+++ index.php
@@ -4,12 +4,11 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+    <?php include("templates/header.php") ?>
+
     <h1>Ninja pizza</h1>
-</body>
+
+    <?php include("templates/footer.php") ?>
+
 </html>
