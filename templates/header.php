aa081e37d9b341136f213b106c61ea080654a612
Dariusz Okonski <darek200180@gmail.com>
2020-09-26 15:56:47 +0100

    add form and styles

--- templates/header.php
+++ templates/header.php
@@ -9,8 +9,15 @@
         .brand {
             background: #cbb09c !important;
         }
+        
         .brand-text {
             color: #cbb09c !important;
+        }
+
+        form {
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
         }
     </style>
 </head>
